019dc0acef1b2a784083e44d1374860d393830fe
Usman Ali <168332899+usmancout@users.noreply.github.com>
2025-07-03 00:26:15 +0500

    updated server

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.use('/api/auth', routes);
 // ✅ Fallback CORS headers on all responses (including errors)
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://endearing-crostata-89c2a0.netlify.app');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
