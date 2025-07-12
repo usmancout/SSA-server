286620541fb554944a6cb5454047b3d14737c017
Usman Ali <usman.cout@gmail.com>
2025-07-13 01:44:22 +0500

    check

--- server.js
+++ server.js
@@ -7,24 +7,21 @@ const routes = require('./router/auth-router');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://endearing-crostata-89c2a0.netlify.app'
-];
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
 });
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://endearing-crostata-89c2a0.netlify.app'
+  ],
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use('/api/auth', routes);
