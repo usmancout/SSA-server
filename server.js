6d40cdb181f0465db0ee8aed428ddea3dbd0ac08
Usman Ali <usman.cout@gmail.com>
2025-07-13 01:08:23 +0500

    jskbd

--- server.js
+++ server.js
@@ -7,17 +7,25 @@ const routes = require('./router/auth-router');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-  origin: [
-    'http://localhost:5173',
-    'https://endearing-crostata-89c2a0.netlify.app/'
-  ],
-  methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  allowedHeaders: 'Content-Type, Authorization',
-  credentials: true,
-};
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://endearing-crostata-89c2a0.netlify.app'
+];
 
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 app.use('/api/auth', routes);
 app.use((req, res) => {
